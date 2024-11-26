9ca9bb2cac8d1e55d01671af6042759f8c5b4639
Ellen696 <00001118363243sp@al.educacao.sp.gov.br>
2024-11-26 20:29:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Disney',
+    'Qual era o hobby favorito de merida(valente)? ',
+    'Atirar com arco e flecha'
+)
+
+criaCartao(
+    'Disney',
+    'Em que ano foi lançado o filme branca de neve e os sete anões?',
+    'Em 1937'
+)
+
+criaCartao(
+    'Disney',
+    'Qual era o sonho da princesa rapunzel no filme enrolados?',
+    'Ver as lanternas flutuantes'
+)
+
+criaCartao(
+    'Disney',
+    'Como Alice chegou ao país das maravilhas?',
+    'Seguindo o coelho branco e caindo em um buraco'
+)
